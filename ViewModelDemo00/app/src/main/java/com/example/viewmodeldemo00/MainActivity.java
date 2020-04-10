3f179652b3b236893843921d538af3d59943f6e5
Daniel Espinosa <d111092.de@gmail.com>
2020-04-10 18:58:03 -0500

    Counter ready, ViewModel not build yet

--- ViewModelDemo00/app/src/main/java/com/example/viewmodeldemo00/MainActivity.java
+++ ViewModelDemo00/app/src/main/java/com/example/viewmodeldemo00/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.viewmodeldemo00;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView textViewCounter;
+    public Button buttonAdd;
+
+    public Integer counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindUI();
+        setCounter();
+    }
+
+    public void bindUI() {
+        textViewCounter = findViewById(R.id.mainTextViewCounter);
+        buttonAdd = findViewById(R.id.mainButtonAdd);
+    }
+
+    public void addONe(View view) {
+        counter++;
+        setCounter();
+    }
+
+    public void setCounter() {
+        textViewCounter.setText(String.valueOf(counter));
     }
 }
