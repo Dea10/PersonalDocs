37318bca56de897e811e7a6794b5efee27da72f7
Daniel Espinosa <d111092.de@gmail.com>
2020-05-28 20:20:37 -0500

    Tipos de datos

--- KotlinBasics/src/app.kt
+++ KotlinBasics/src/app.kt
@@ -1,6 +1,6 @@
 fun main() {
-    // Variables y Constantes
-    variablesYConstantes()
+    // variablesYConstantes()
+    tiposDeDatos()
 }
 
 private fun variablesYConstantes() {
@@ -30,3 +30,35 @@ private fun variablesYConstantes() {
     println(mySecondConst)
 }
 
+private fun tiposDeDatos() {
+    println("*** Tipos de Datos ***")
+
+    // String
+    val myString : String = "Hello"
+    val myString02 = "Daniel Espinosa"
+    val myString03 = myString + " " + myString02
+    val myString04 = "${myString} ${myString02}"
+    println(myString03)
+    println(myString04)
+
+    // Enteros (Byte, Short, Int, Long)
+    val myInt : Int = 1
+    val myInt02 = 2
+    val myInt03 = myInt + myInt02
+    println(myInt03)
+    println("${myInt} + ${myInt02} = ${myInt + myInt02}")
+
+    // Decimales (Float, Double)
+    val myFloat : Float = 1.5f
+    val myDouble : Double = 1.5
+    val myDouble02 = 2.6
+    val myDouble03 = 1 // Este es Int, pero al momento de operar lo convierte en Double
+    println("${myDouble} + ${myDouble02} + ${myDouble03} = ${myDouble + myDouble02 + myDouble03}")
+
+    // Boolean (Bool)
+    val myBool : Boolean = true
+    val myBool02 = false
+    //val myBool03 = myBool + myBool02 Operaci[on no permitida con Bool
+    println("${myBool} == ${myBool02} = ${myBool == myBool02}")
+    println("${myBool} || ${myBool02} = ${myBool || myBool02}")
+}
